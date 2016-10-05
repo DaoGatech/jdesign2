a07b6f50ffd4b1a94ebf097cc04ee3f465177f8d
Nimish Todi <Nimish Todi>
2016-10-05 01:07:19 -0400

    Updated events to highlight any events on the current day

--- src/src/client/app/events/events.component.e2e-spec.ts
+++ src/src/client/app/events/events.component.e2e-spec.ts
@@ -4,7 +4,7 @@ describe('Events', () => {
     browser.get('/events');
   });
 
-  it('should have correct feature heading', () => {
+  it('should have correct feature heading', () => { 
     expect(element(by.css('sd-events h2')).getText()).toEqual('Features');
   });
 
---
+++ src/src/client/app/events/events.currentday.js
@@ -0,0 +1,16 @@
+setTimeout(function(){
+	var today = new Date();
+	var dd = today.getDate();
+	var mm = today.getMonth()+1; //January is 0!
+	var yyyy = today.getFullYear()
+	var div = document.getElementsByTagName("span");
+	var events = document.getElementsByTagName("li");
+	todays = []
+	for (i = 0; i < div.length; i++) {
+		var date = div[i].textContent;
+		var datesplit = date.split("/");
+		if (datesplit[1] == dd && datesplit[0] == mm) {
+			events[i].style.backgroundColor = "green";
+		}
+	}
+},4001);
