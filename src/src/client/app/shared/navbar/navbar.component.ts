a4f0e6ef4a65036cda257d9567f9a0a81abd8056
Jack Li <jack1337li@gmail.com>
2016-10-05 08:26:07 -0400

    fixed navbar current page bug

--- src/src/client/app/shared/navbar/navbar.component.ts
+++ src/src/client/app/shared/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 /**
  * This class represents the navigation bar component.
  */
@@ -15,10 +16,20 @@ export class NavbarComponent {
 	test: string = "testing";
 	isActive: boolean = false;
 	navItems: Array<String> = ["Events","Stats","Contacts"];
-	navItemActive: number = 0;
+	navItemActive: number;
 
 
-	constructor(){
+	constructor(private _router: Location){
+
+		this.navItemActive = 0;
+		if (_router.path() === "/Events"){
+			this.navItemActive = 1;
+		} else if (_router.path() === "/Stats"){
+			this.navItemActive = 2;
+		} else if (_router.path() === "/Contacts"){
+			this.navItemActive = 3;
+		}
+		
 		for(var i = 0; i < this.navItems.length; i++){
 			console.log(this.navItems[i]);
 		}
