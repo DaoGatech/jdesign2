4f229bfe5f96a98d51d099470b4f747279921716
tabney00 <tabney00@gmail.com>
2016-11-17 20:25:54 -0500

    finishing up backend

--- backend.py
+++ backend.py
@@ -1,12 +1,12 @@
-import pandas as pd, json
+import pandas as pd, json, math
 from datetime import datetime, date, time
 
-jsonFilename = 'crcJson.json'
+crcFilename = 'crcJson.json'
 dateFilename = 'date.json'
 activeFilename = 'active.json'
 
 try:
-    with open( jsonFilename ) as areasfile:        
+    with open( crcFilename ) as areasfile:        
         Areas = json.load( areasfile )
 except IOError:
     Areas = {}
@@ -14,9 +14,10 @@ except IOError:
 try:
     with open( activeFilename ) as activefile:        
         activeAreas = json.load( activefile )
+        print activeAreas[0]
+        activeAreas[0] = datetime.strptime( activeAreas[0], '%M-%H-%d-%m-%Y' )
 except IOError:
-    activeAreas = {}
-
+    activeAreas = [ datetime( 1970, 1, 1), {} ]
 
 
 #read excel file and build data structure (starting with Areas)
@@ -47,29 +48,40 @@ def pull():
           	#add crc area or append to it
             if cell[0] not in Areas.keys():
                 Areas[cell[0]] = {}
-            else:
-                weekday = parsedate( rowDate[1] )
-                daydict = Areas[cell[0]]
-                
-                #add day or append to it
-                if weekday not in daydict.keys():
-                    daydict[weekday] = {}
-                else:
-                    lightdict = daydict[weekday]
-                    light = getLight( time[1] )
-                    
-                    #add time of day or append to it based on time
-                    if light not in lightdict.keys():
-                        lightdict[light] = []
-                    else:
-                        time_and_occs = lightdict[light]
-                        time_and_occs.append( ( time[1], cell[1] ) )
 
+            weekday = parsedate( rowDate[1] )
+            daydict = Areas[cell[0]]
+                
+            #add day or append to it
+            if weekday not in daydict.keys():
+                daydict[weekday] = {}
+
+            lightdict = daydict[weekday]
+            light = getLight( time[1] )
+                
+            #add time of day or append to it based on time
+            if light not in lightdict.keys():
+                lightdict[light] = []
+
+            time_and_occs = lightdict[light]
+            
+            if not math.isnan( cell[1] ) and isinstance( cell[1], float ):
+                time_and_occs.append( ( time[1], cell[1] ) )
+
+            activeAreas[0] = addStringToDT( rowDate[1], time[1] )
+            
+            if cell[0] not in activeAreas[1].keys():
+                activeAreas[1][cell[0]] = {}
+            if not math.isnan( cell[1] ) and isinstance( cell[1], float ):
+                activeAreas[1][cell[0]] = cell[1]
+
+    with open( activeFilename, 'w' ) as fp:
+        json.dump( [activeAreas[0].strftime( "%M-%H-%d-%m-%Y" ), activeAreas[1]], fp )
 
     with open( dateFilename, 'w' ) as fp:
         json.dump(addStringToDT( rowDate[1], time[1]).strftime( "%M-%H-%d-%m-%Y" ), fp )
     
-    with open( jsonFilename, 'w' ) as fp:
+    with open( crcFilename, 'w' ) as fp:
       json.dump( Areas, fp )
 
 
