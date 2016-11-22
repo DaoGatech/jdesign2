e1af06eeccfbaf2d623302f89169ecbd92177fa8
Royce <xroycekimx@gmail.com>
2016-11-21 22:29:27 -0500

    Moved legend to right side

--- src/src/client/app/stats/stats.component.ts
+++ src/src/client/app/stats/stats.component.ts
@@ -76,6 +76,9 @@ export class StatsComponent implements OnInit {
                       }
                   }]
               },
+              legend: {
+                position: "right"
+              }
           }
       });
     }
