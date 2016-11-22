2cb5a687e7bd23e92dc0ed9f3840563d1b8d756e
Royce <xroycekimx@gmail.com>
2016-11-21 19:22:38 -0500

    First attempt at graph for stats

--- src/src/client/app/stats/stats.component.ts
+++ src/src/client/app/stats/stats.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 /**
  * This class represents the lazy loaded StatsComponent.
@@ -10,18 +10,51 @@ import { Component } from '@angular/core';
   styleUrls: ['stats.component.css']
 })
 
-export class StatsComponent {
+export class StatsComponent implements OnInit {
     statsAreas: Array<String> = ["Indoor Pool", "Basketball Court", "Outdoor Pool"];
     // statsMonths: Array<String> = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     statsRange: Array<String> = ["24 hours", "Week", "Month"]
     statsDays: Array<String> = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     statsTimes: Array<String> = ["Morning", "Afternoon", "Evening", "Late Night"];
 
-
   	selectedDevice = 'Indoor Pool';
+
+    randomData: Array<number> = [12, 19, 3, 5, 2, 3];
+    title: String = '# of Fucks I Give';
+    tickLabels: Array<String> = ["WHAT", "THE", "FUCK", "AM", "I", "DOING"];
+
+    ngOnInit() {
+      var ctx = document.getElementById("myChart");
+      var myChart = new Chart(ctx, {
+          type: 'line',
+          data: {
+              labels: this.tickLabels,
+              datasets: [{
+                  label: this.title,
+                  data: this.randomData,
+                  backgroundColor: [
+                      'rgba(255, 99, 132, 0.2)',
+                      ],
+                  borderColor: [
+                      'rgba(255,99,132,1)',
+                      ],
+                  borderWidth: 3
+              }]
+          },
+          options: {
+              scales: {
+                  yAxes: [{
+                      ticks: {
+                          beginAtZero:true
+                      }
+                  }]
+              }
+          }
+      });
+    }
+
   	onChange(newValue: any) {
     	console.log(newValue);
-
     	this.selectedDevice = newValue;
-	}
+	  }
 }
--- src/src/client/app/stats/stats.module.ts
+++ src/src/client/app/stats/stats.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StatsComponent } from './stats.component';
 import { FormsModule } from '@angular/forms';
+import { ChartsModule } from 'ng2-charts/ng2-charts';
 
 @NgModule({
-    imports: [CommonModule, FormsModule],
+    imports: [CommonModule, FormsModule, ChartsModule],
     declarations: [StatsComponent],
     exports: [StatsComponent]
 })
